2892eb37c01cba42e7728d22f553fff7b06d945e
yemisrach <meserlengereh@gmail.com>
2017-11-01 12:38:38 -0400

    Initial commit

---
+++ src/main/java/com/example/bootstr/BootstrApplication.java
@@ -0,0 +1,12 @@
+package com.example.bootstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrApplication.class, args);
+	}
+}
